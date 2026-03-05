8f9a0a2f7b71668efe085e2e81fa1bb928be29d8
Pylimas <kristina.db@hotmail.com>
2026-03-05 17:57:18 +0200

    .

---
+++ db/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsename="root";
+$dbPassword="";
+$dbName="loginsystem";
+$dbPort=3307;
+
+$conn = mysqli_connect($dbServername, $dbUsename, $dbPassword, $dbName,$dbPort);
+
+?>
