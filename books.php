c6f4c84b7dec014dc92a22e4e604cc1ac3e7356e
Pylimas <kristina.db@hotmail.com>
2026-03-05 18:29:09 +0200

    showing data in table

--- books.php
+++ books.php
@@ -1,3 +1,7 @@
+<?php 
+include_once "dbh.inc.php"
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,11 +9,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="style.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" 
+    crossorigin="anonymous">
 </head>
 <body>
     <?php require_once "header.php"?>
     <main>
-
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Book name</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Isbn</th>
+                    <th scope="col">Quantity in library</th>
+                    <th scope="col">Free books</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php 
+                    $sql = "SELECT * FROM book;";
+                    $result = mysqli_query($conn, $sql);
+                    $resultCheck = mysqli_num_rows($result);
+                    if($resultCheck>0){
+                        while ($row = mysqli_fetch_assoc($result)){
+                            echo '<tr>
+                            <th>'.$row['id'].'</th>
+                            <td>'.$row['name'].'</td>
+                            <td>'.$row['author'].'</td>
+                            <td>'.$row['isbn'].'</td>
+                            <td>'.$row['quantity'].'</td>
+                            <td>'.$row['free'].'</td>
+                            </tr>';
+                        }
+                    }
+                ?>
+            </tbody>
+        </table>
     </main>
 </body>
 </html>
