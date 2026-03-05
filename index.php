f717c74410dc5c2836ac61b49a72e6c27e0cb338
Pylimas <kristina.db@hotmail.com>
2026-03-05 13:54:44 +0200

    changing file type

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+
+    </main>
+</body>
+</html>
+<!-- https://stackoverflow.com/questions/41330707/how-can-i-format-php-files-with-html-markup-in-visual-studio-code -->
