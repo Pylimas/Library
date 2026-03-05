1787ce66dc0227b512d0211cff31e6050a5bc085
Pylimas <kristina.db@hotmail.com>
2026-03-05 15:02:46 +0200

    menu

---
+++ header.php
@@ -0,0 +1,8 @@
+<header>
+    <nav>
+        <a href="index.php">Main page</a>
+        <a href="books.php">Books</a>
+        <a href="readers.php">Readers</a>
+        <a href="employee.php">Employees</a>
+    </nav>
+</header>
