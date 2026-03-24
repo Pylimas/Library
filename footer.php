daacbcbd0d3999bf2b59d97e62c605ae2335ed2c
Pylimas <kristina.db@hotmail.com>
2026-03-24 22:09:12 +0200

    footer structure

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <b class="f_name">Lorem ipsum</b>
+    <div class="f_container">
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+        <p>Lorem ipsum</p>
+    </div>
+</footer>
