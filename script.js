021dd64baa340d863e96911a3aab43c1ce81c2f5
Pylimas <kristina.db@hotmail.com>
2026-03-27 15:26:36 +0200

    form show function

--- script.js
+++ script.js
@@ -9,3 +9,12 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
   carousel.scrollLeft -= 200;
 });
+
+function showAddBookForm(){
+  var form = document.getElementById("addBook");
+  if(form.style.display === "none"){
+    form.style.display = "block";
+  }else{
+    form.style.display = "none";
+  }
+}
