37bbb116c3ea5642e674b86c059dd7071d44036a
Pylimas <kristina.db@hotmail.com>
2026-03-27 18:43:38 +0200

    passing book info to form

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function showAddBookForm(){
     form.style.display = "none";
   }
 }
+
+function showUpdateBookForm(id){
+  var form = document.getElementById("updateBook");
+  if(form.style.display === "none"){
+    form.style.display = "block";
+    document.querySelector('#updateBook input[name="id"]').value = id;
+    fetch('getBook.php?id=' + id)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('updateName').value = data.book_name;
+            document.getElementById('updateAuthor').value = data.author;
+            document.getElementById('updateIsbn').value = data.isbn;
+            document.getElementById('updateQuantity').value = data.quantity;
+        });
+  }else{
+    form.style.display = "none";
+  }
+}
