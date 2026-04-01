56e409af259104c6b989274e1079d2c7f656248a
Pylimas <kristina.db@hotmail.com>
2026-04-01 21:07:12 +0300

    deleting from takeb books and substracting from book

---
+++ returnBook.php
@@ -0,0 +1,24 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $id = $_POST['id'];
+    $book_id = $_POST['book_id'];
+    try{
+        require_once "dbh.inc.php";
+        $query = "DELETE FROM taken_books where id = $id;";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute();
+
+        $query = "SELECT taken FROM book WHERE id = $book_id;";
+        $query = "UPDATE book SET taken=taken-1 where id = $book_id;";
+
+        $pdo=null;
+        $stmt=null;
+        header("Location: readers.php");
+        exit();
+    }catch(PDOException $e){
+        die("Query failed: ". $e->getMessage());
+    }
+}
+
+?>
