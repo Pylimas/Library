d5b83c8ba068aaa735bcb0a7dfe665039848650a
Pylimas <kristina.db@hotmail.com>
2026-04-16 12:59:08 +0300

    adding employee

---
+++ addEmployee.php
@@ -0,0 +1,24 @@
+<?php
+if($_SERVER["REQUEST_METHOD"=="POST"]){
+    $name=$_POST["name"];
+    $surname=$_POST["surname"];
+    $username=$_POST["username"];
+    $pass=$_POST["pass"];
+    try{
+        require_once "dbh.inc.php";
+        $query = "INSERT INTO employee (employee_name, surname, username, pass) VALUES (?,?,?,?);";
+        $stmt = $pdo->prepare($query);
+        $stmt->execute([$name, $surname, $username, $pass]);
+        $pdo=null;
+        $stmt = null;
+        header("Location: employee.php");
+        exit();
+    }catch(PDOException $e){
+        die("Query failed: ". $e->getMessage());
+    }
+}else{
+    header("Location: employee.php");
+    exit();
+}
+
+?>
